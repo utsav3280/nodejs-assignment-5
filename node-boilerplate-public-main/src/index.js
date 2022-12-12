c3ab9574f4ff961d4d88f420cab0d6bdae409900
Utsav <utsav.s3280@gmail.com>
2022-12-12 17:13:26 +0530

    uploaded assignment-5

---
+++ node-boilerplate-public-main/src/index.js
@@ -0,0 +1,29 @@
+var http = require("http");
+
+const httpServer = http.createServer(handleServer);
+
+function handleServer(req, res) {
+    if (req.url == "/welcome") {
+        res.writeHead(200, { "Content-type": "text/plain" })
+        res.write("Welcome to Dominos!");
+        res.end();
+    }
+    else if (req.url == "/contact") {
+        res.writeHead(200, { "Content-type": "application/json" })
+        res.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }
+        ));
+        res.end();
+    }
+    else {
+        res.writeHead(404, { "Content-type": "text/plain" });
+        res.write("Page not found");
+        res.end();
+    }
+}
+
+httpServer.listen(8081, () => { console.log("Server is up at 8081") })
+
+module.exports = httpServer;
